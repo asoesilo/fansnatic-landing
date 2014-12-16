3ab75736bab4016a6ce201784552f66115333357
Andre Soesilo <asoesilo84@gmail.com>
2014-12-15 21:29:27 -0800

    MOdify user create endpoint to send 400 only when it is not a duplicate user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,20 @@ class UsersController < ApplicationController
   def create
     email = params['email']
     user = User.create email: email
-    return head 400 unless user.valid?
 
-    Mailer.send_email email
+    unless user.valid?
+      messages = user.errors.messages
+      isMoreThanOneError = messages.length > 1
+      isNotEmailError = messages[:email].length == 0
+      isNotDuplicateError = messages[:email].length > 1 || messages[:email][0] != "has already been taken"
+
+      if isMoreThanOneError || isNotEmailError || isNotDuplicateError
+        puts "Errors: #{messages}"
+        head 400
+      end
+    else
+      Mailer.send_email email
+    end
     head :ok
   end
 end
